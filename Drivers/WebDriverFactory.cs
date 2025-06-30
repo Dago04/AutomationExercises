c280c033a1f361bb01d8c586cf1aab62dc142b3d
Dago Salas <DagoSalas1999@gmail.com>
2025-06-29 19:22:04 -0600

    Se crea una carpeta Drivers donde se almacena la clase creadora del driver de chrome

---
+++ Drivers/WebDriverFactory.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace AutomationExercises.Drivers
+{
+    public class WebDriverFactory
+    {
+
+        public static IWebDriver CreateDriver(bool headless = false) { 
+        
+
+            ChromeOptions options = new ChromeOptions();
+
+            if (headless)
+            { 
+                options.AddArgument("--headless");
+                options.AddArgument("--disable-gpu");
+                options.AddArgument("--window-size=1920,1080");
+            }
+
+            IWebDriver driver = new ChromeDriver(options);
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            return driver;
+
+        }
+    }
+}
